9aef923e2f05305c376f47738d6ffdb29454757c
Kyle <Ksuckow1@luc.edu>
2018-02-28 01:12:24 -0600

    I have no idea what I'm doing

---
+++ Greetinggen.js
@@ -0,0 +1,8 @@
+var buttonvar = document.getElementById("button");
+buttonvar.addEventListener("click", function(){
+  var input=document.getelementbyid("input").value;
+var greetings = [
+  "hello" input "how are you?"
+  "zdravo" input "kako si?"
+  "Bonjour" input "Comment allez-vous?"
+ ]
